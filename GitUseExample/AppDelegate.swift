82a20304f60c9e39a7cbfca48cd874c206b4f8b9
Bhumisjm <bhumisjm@gmail.com>
2016-02-18 15:52:29 +0530

    com

--- GitUseExample/AppDelegate.swift
+++ GitUseExample/AppDelegate.swift
@@ -12,12 +12,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         print("finish")
-        
+        print("On Githib")
         // Override point for customization after application launch.
         return true
     }
